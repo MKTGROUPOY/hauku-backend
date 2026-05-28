52f1eb1e8f6e541f160f19472ca2230f109450d9
MKTGROUPOY <tallbergmkm@gmail.com>
2026-05-28 14:53:05 +0300

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,18 +1,28 @@
-import { getProducts } from '../lib/shopify.js';
-
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const products = await getProducts();
-  
-  const hasLa = products.filter(p => p.la && p.la.length > 5).length;
-  const hasRv = products.filter(p => p.rv && p.rv.length > 5).length;
-  const hasA = products.filter(p => p.a && p.a.length > 5).length;
-  
-  res.status(200).json({
-    total: products.length,
-    hasLisaaineet: hasLa,
-    hasRavintoaineet: hasRv,
-    hasAinesosat: hasA,
-    pctLisaaineet: Math.round(100*hasLa/products.length) + '%'
+  const shopDomain = process.env.SHOPIFY_DOMAIN;
+  const token = process.env.SHOPIFY_PUBLIC_TOKEN;
+
+  const query = `{
+    products(first: 1, query: "title:\\"Grandorf Fresh Adult Medium\\"") {
+      edges {
+        node {
+          title
+          metafields(identifiers: [
+            {namespace:"custom", key:"lisaaineet"}
+            {namespace:"custom", key:"ravintoaineet"}
+            {namespace:"custom", key:"ainesosat"}
+          ]) { key value }
+        }
+      }
+    }
+  }`;
+
+  const r = await fetch(`https://${shopDomain}/api/2025-07/graphql.json`, {
+    method: 'POST',
+    headers: {'Content-Type':'application/json','X-Shopify-Storefront-Access-Token':token},
+    body: JSON.stringify({query}),
   });
+  const data = await r.json();
+  res.status(200).json(data);
 }
